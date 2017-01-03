e24d1a45f80c2a9573f67dfa0e7bffcea030f252
Matej Lachman <matej.lachman@email.cz>
2017-01-03 23:51:11 +0100

    Just added simple while cycle example I wrote to not fall asleep due to boringness of initial lectures.

---
+++ while_example.py
@@ -0,0 +1,8 @@
+#Simple while example, just for not falling asleep while watching lextures
+
+name = ""
+
+while name != "Matej":
+    print("Please tell me name of the most handsome dude ever.")
+    name = input()
+print("Was not that hard, right?")
