ac032cdeafb71cace726ec961b2fae70a2f069af
Matej Lachman <matej.lachman@email.cz>
2017-01-05 14:30:42 +0100

    Jen mini-program rekapitulujici 8. lekci

---
+++ importy.py
@@ -0,0 +1,9 @@
+import pyperclip, sys
+
+pyperclip.copy("Spam and Eggs!")
+print("Whats going on?")
+pyperclip.paste()
+print(pyperclip.paste())
+print("I have no idea")
+sys.exit()
+print("This will not happen")
